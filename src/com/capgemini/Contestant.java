a685f29d04f6809f13816e73a13d35b992dd3612
DavidMijdam <mijdam1996@gmail.com>
2018-09-18 10:16:08 +0200

    [ADD] Added a Rest and Sharpen method to the base Contestant class.

--- src/com/capgemini/Contestant.java
+++ src/com/capgemini/Contestant.java
@@ -7,4 +7,14 @@ public class Contestant {
     int defense;
     String name;
 
+    void rest(){
+        System.out.println(name + " takes a nap and restores 20 HP");
+        health += 20;
+    }
+
+    void sharpen(){
+        System.out.println(name + " sharpens his weapon and gains a +1 attack bonus");
+        attack += 1;
+    }
+
 }
