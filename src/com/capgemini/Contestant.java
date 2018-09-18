0f7a84c6bd73aa8bf7f761373929df588e17b6c3
DavidMijdam <mijdam1996@gmail.com>
2018-09-18 10:42:49 +0200

    [ADD] Contestants can hurt other contestants.

--- src/com/capgemini/Contestant.java
+++ src/com/capgemini/Contestant.java
@@ -30,14 +30,32 @@ public class Contestant {
         }
     }
 
+    //Action that allows contestants to heal.
     void rest(){
-        System.out.println(name + " takes a nap and restores 20 HP");
+        System.out.println(name + " takes a nap and restores 20 HP.");
         health += 20;
     }
 
+    // Action that raises attack.
     void sharpen(){
-        System.out.println(name + " sharpens his weapon and gains a +1 attack bonus");
+        System.out.println(name + " sharpens his weapon and gains a +1 attack bonus.");
         attack += 1;
+    }
+
+    //Action that does nothing.
+    void hide(){
+        System.out.println(name + " hides until potential threats are gone.");
+    }
+
+    void inflictDamage(Contestant victim){
+        int damage = calculateDamage(victim);
+        System.out.println(name + " slashes at " + victim.name + " and inflicts " + damage + " damage.");
+        victim.health -= damage;
+    }
+
+    int calculateDamage(Contestant victim){
+        int damageDealt = (attack * 10) - (victim.defense * 2);
+        return damageDealt;
     }
 
 }
--- src/com/capgemini/Main.java
+++ src/com/capgemini/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	    CareerContestant richard = new CareerContestant("Richard");
+	    DistrictContestant maggy = new DistrictContestant("Maggy");
 
 	    System.out.println(richard.name + " has an attack stat of " + richard.attack);
+	    richard.inflictDamage(maggy);
+        System.out.println(maggy.health);
     }
 }
